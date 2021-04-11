401d67eb95091fdf0b0ecf8a3b763d66f2fa7414
MK-2012 <70899588+MK-2012@users.noreply.github.com>
2021-04-11 19:56:24 +0300

    Delete game/build directory

--- game/build/src/CMakeFiles/Cursor.dir/PlayerEnum.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/Cursor.dir/PlayerEnum.cpp.o: \
- /home/mikhail_2012/TP-Game/game/src/PlayerEnum.cpp \
- /usr/include/stdc-predef.h \
- /home/mikhail_2012/TP-Game/game/include/PlayerEnum.h
--- game/build/src/CMakeFiles/Treasury.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Treasury.
-# This may be replaced when dependencies are built.
